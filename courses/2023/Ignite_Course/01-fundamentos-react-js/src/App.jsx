02fa46571bf8a9d6486c12532c1bfc522baef5b8
italo.amaral <italorockamaral@gmail.com>
2023-05-31 09:09:01 -0300

    feat: sidebar component

--- courses/2023/Ignite_Course/01-fundamentos-react-js/src/App.jsx
+++ courses/2023/Ignite_Course/01-fundamentos-react-js/src/App.jsx
@@ -1,15 +1,23 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 import "./global.css"
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Italo"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ullam odio deleniti asperiores quasi repellat, repudiandae, modi dolor a animi voluptas aliquid in explicabo molestiae sequi veniam voluptatem placeat. Fugit!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Italo"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ullam odio deleniti asperiores quasi repellat, repudiandae, modi dolor a animi voluptas aliquid in explicabo molestiae sequi veniam voluptatem placeat. Fugit!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
